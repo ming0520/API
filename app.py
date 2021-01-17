7c7b97e79d5069f96953155ba397edc9c5d6dccc
Tan Zhong Ming <mingblog0520@gmail.com>
2021-01-17 20:37:06 +0800

    Update app.py
    add threaded

--- app.py
+++ app.py
@@ -233,6 +233,6 @@ def uploaded_file(filename):
     # return send_from_directory(app.config['UPLOAD_FOLDER'],
     #                             filename)
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
-    # app.run(host='0.0.0.0',port=80)
+    # app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0',port=80, threaded=True)
 
