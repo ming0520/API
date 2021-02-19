5326e40c3b1bb840effba23435471dec8308934e
Tan Zhong Ming <mingblog0520@gmail.com>
2021-02-19 19:56:01 +0800

    resolve conflixt

--- app.py
+++ app.py
@@ -302,10 +302,6 @@ def uploaded_file(filename):
     # return send_from_directory(app.config['UPLOAD_FOLDER'],
     #                             filename)
 if __name__ == "__main__":
-<<<<<<< HEAD
-    #app.run(debug=True, threaded=True)
-=======
     # app.run(debug=True, threaded=True)
->>>>>>> 2f9f33ec1c4e3e1628ca4c8d180dd668bdfb93b3
     app.run(host='0.0.0.0',port=80, threaded=True)
 
