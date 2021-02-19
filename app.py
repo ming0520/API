574f663adff78805296630793526e11e955459e4
ming0520 <mingblog0520@gmail.com>
2021-02-19 11:54:34 +0000

    update from server

--- app.py
+++ app.py
@@ -302,6 +302,6 @@ def uploaded_file(filename):
     # return send_from_directory(app.config['UPLOAD_FOLDER'],
     #                             filename)
 if __name__ == "__main__":
-    app.run(debug=True, threaded=True)
-    # app.run(host='0.0.0.0',port=80, threaded=True)
+    #app.run(debug=True, threaded=True)
+    app.run(host='0.0.0.0',port=80, threaded=True)
 
